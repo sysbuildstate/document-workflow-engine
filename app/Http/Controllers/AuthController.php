fa027f7b47203609b38aa9f4376feeb3a85a120c
olivia lauar <olivialauarg@gmail.com>
2026-07-28 20:42:37 -0300

    fix: auto-create missing spatie rbac roles during user registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Spatie\Permission\Models\Role;
 
 class AuthController extends Controller
 {
@@ -27,6 +28,7 @@ class AuthController extends Controller
         ]);
 
         if (!empty($validated['role'])) {
+            Role::firstOrCreate(['name' => $validated['role']]);
             $user->assignRole($validated['role']);
         }
 
